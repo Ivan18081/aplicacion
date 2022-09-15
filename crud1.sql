b1ccbd095e54b55a3dbc4dd72eb82b0bbdef0b08
Ivan18081 <112780092+Ivan18081@users.noreply.github.com>
2022-09-15 17:07:35 -0500

    Update crud1.sql

--- crud1.sql
+++ crud1.sql
@@ -1,9 +1,9 @@
 create database crud1;
 use crud1;
 
-drop table if exist person;
+drop table if exist `person`;
 
-create table person(
+create table `person`(
 	id int not null auto_increment primary key,
 	name varchar(500) not null,
 	lastname varchar(500) not null,
